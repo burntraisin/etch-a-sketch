1b6d9c1780bcceeb92e4aab390b9a7d672f335a2
burntraisin <rbkehan@gmail.com>
2023-03-06 06:40:44 -0500

    Create 16 squares

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const container = document.querySelector(".grid");
-const div = document.createElement("div");
-div.style.backgroundColor = "black";
-div.style.width = "10px";
-div.style.height = "10px";
-container.appendChild(div);
+let widthValue = 16;
 
+function createCanvas(widthValue) {
+    for (let i = 0; i < widthValue; i += 1) {
+        const div = document.createElement("div");
+        div.style.backgroundColor = "black";
+        div.style.width = "10px";
+        div.style.height = "10px";
+        container.appendChild(div);
+    }
+}
+
+createCanvas(widthValue);
