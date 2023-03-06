92dacfeeb2f3b67b66a07a3f81fa5ab982c9c5e7
burntraisin <rbkehan@gmail.com>
2023-03-06 06:25:38 -0500

    Create 1 grid square

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".grid");
+const div = document.createElement("div");
+div.style.backgroundColor = "black";
+div.style.width = "10px";
+div.style.height = "10px";
+container.appendChild(div);
+
