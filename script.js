98cf7c8413adf93852b51a203dea3e1bd159f684
burntraisin <rbkehan@gmail.com>
2023-03-09 16:26:02 -0500

    Delete comment

--- script.js
+++ script.js
@@ -37,29 +37,3 @@ slider.onchange = function() {
     let canvasDimensions = document.querySelector(".slider").value;
     canvas(canvasDimensions);
 }
-
-// ETCH A SKETCH CANVAS
-
-const container = document.querySelector(".grid");
-
-let gridUnitSize = "1fr ";
-let gridSize = gridUnitSize.repeat(canvasDimensions).trimEnd(); // 1fr per 1 unit of canvasDimensions
-
-container.style.width = "600px";
-container.style.height = "600px";
-container.style.display = "grid";
-container.style.backgroundColor = "pink";
-container.style.gridTemplateColumns = gridSize;
-container.style.gridTemplateRows = gridSize;
-
-// CHANGE COLOR ON HOVER
-
-// Creates a div for each grid item and changes its color when mouseover
-for (let i = 0; i < canvasDimensions * canvasDimensions; i++) {
-    const gridChild = document.createElement("div");
-    gridChild.classList.add("grid-child");
-    container.appendChild(gridChild);
-    gridChild.addEventListener("mouseover", () => {
-        gridChild.classList.add("active");
-    });
-}
