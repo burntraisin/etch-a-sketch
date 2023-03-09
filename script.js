9592c3a5da02fdaefc8eb2f26577e4e46bf654a2
burntraisin <rbkehan@gmail.com>
2023-03-09 16:23:53 -0500

    Complete canvas resize and refresh based on slider

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-// CANVAS SIZE
+function canvas(canvasDimensions) {
+    const container = document.querySelector(".grid");
+
+    let gridUnitSize = "1fr ";
+    let gridSize = gridUnitSize.repeat(canvasDimensions).trimEnd(); // 1fr per 1 unit of canvasDimensions
+
+    container.style.width = "600px";
+    container.style.height = "600px";
+    container.style.display = "grid";
+    container.style.backgroundColor = "pink";
+    container.style.gridTemplateColumns = gridSize;
+    container.style.gridTemplateRows = gridSize;
+
+    // Creates a div for each grid item and changes its color when mouseover
+    for (let i = 0; i < canvasDimensions * canvasDimensions; i++) {
+        const gridChild = document.createElement("div");
+        gridChild.classList.add("grid-child");
+        container.appendChild(gridChild);
+        gridChild.addEventListener("mouseover", () => {
+            gridChild.classList.add("active");
+        });
+    }
+
+    // Clears previous painting when changing canvas size
+    const activeGridChild = document.querySelectorAll(".grid-child");
+    activeGridChild.forEach((gridChild) => {
+        gridChild.classList.remove("active");
+    });
+
+}
 
 const slider = document.querySelector(".slider");
 let canvasDimensions = document.querySelector(".slider").value;
+canvas(canvasDimensions);
+
 slider.onchange = function() {
-    canvasDimensions = document.querySelector(".slider").value;
+    let canvasDimensions = document.querySelector(".slider").value;
+    canvas(canvasDimensions);
 }
 
 // ETCH A SKETCH CANVAS
@@ -19,33 +51,6 @@ container.style.display = "grid";
 container.style.backgroundColor = "pink";
 container.style.gridTemplateColumns = gridSize;
 container.style.gridTemplateRows = gridSize;
-
-slider.addEventListener("click", () => {
-    let canvasDimensionsUpdate = document.querySelector(".slider").value;
-    let gridUnitSize = "1fr ";
-    let gridSize = gridUnitSize.repeat(canvasDimensionsUpdate).trimEnd(); // 1fr per 1 unit of canvasDimensions
-
-    container.style.width = "600px";
-    container.style.height = "600px";
-    container.style.display = "grid";
-    container.style.backgroundColor = "pink";
-    container.style.gridTemplateColumns = gridSize;
-    container.style.gridTemplateRows = gridSize;
-
-    /* remove.onclick = () => {
-        gridChild.classList.remove("active");
-    }
- */
-    // Creates a div for each grid item and changes its color when mouseover
-    for (let i = 0; i < canvasDimensionsUpdate * canvasDimensionsUpdate; i++) {
-        const gridChild = document.createElement("div");
-        gridChild.classList.add("grid-child");
-        container.appendChild(gridChild);
-        gridChild.addEventListener("mouseover", () => {
-            gridChild.classList.add("active");
-        });
-    }
-});
 
 // CHANGE COLOR ON HOVER
 
